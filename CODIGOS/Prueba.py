a19f46fe0d4b0bf4cebb40fb2abae61a3f9a7c2a
LuisAguero44 <160888051+LuisAguero44@users.noreply.github.com>
2024-11-22 10:09:01 -0300

    Update Prueba.py

--- CODIGOS/Prueba.py
+++ CODIGOS/Prueba.py
@@ -1,41 +1,58 @@
 import cv2
 import numpy as np
 import tensorflow as tf
-import SeguimientoManos as sm  
+import SeguimientoManos as sm
 
 
 model = tf.keras.models.load_model("modelo_gestos.h5")
 
 
-detector = sm.detectormanos(Confdeteccion=0.8)
+detector = sm.detectormanos(Confdeteccion=0.9)
 
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-class_names = list(model.class_names) 
+class_names = ['1', '2', '3','A', 'B', 'C','L','Te quiero'] #nokmbres de las clases
 
 while True:
-   
+    
     ret, frame = cap.read()
     
+   
     frame = detector.encontrarmanos(frame)
-    lista, bbox = detector.encontrarposicion(frame, dibujarPuntos=False, dibujarBox=True)
+    
+   
+    resultado = detector.encontrarposicion(frame, dibujarPuntos=False, dibujarBox=True)
     
     
+    print(resultado)
+    
+    
+    if len(resultado) == 2:  
+        lista, bbox = resultado
+    elif len(resultado) == 3:  
+        lista, bbox, otro_valor = resultado
+    else:
+        lista, bbox = [], []  
+
+  
     if bbox:
-        
         xmin, ymin, xmax, ymax = bbox
         xmin, ymin = max(0, xmin - 20), max(0, ymin - 20)  
         xmax, ymax = min(frame.shape[1], xmax + 20), min(frame.shape[0], ymax + 20)
         
-        recorte = frame[ymin:ymax, xmin:xmax]
-        recorte = cv2.resize(recorte, (224, 224))  
-        recorte = cv2.cvtColor(recorte, cv2.COLOR_BGR2RGB) 
-        recorte = recorte / 255.0 
         
-        recorte = np.expand_dims(recorte, axis=0) 
+        recorte = frame[ymin:ymax, xmin:xmax]
+        recorte = cv2.resize(recorte, (448,448))  
+        recorte = cv2.cvtColor(recorte, cv2.COLOR_BGR2RGB) 
+        recorte = recorte / 255.0  
+        
+        
+        recorte = np.expand_dims(recorte, axis=0)
+        
+        
         prediccion = model.predict(recorte)
         clase_index = np.argmax(prediccion)
         clase_gesto = class_names[clase_index]
@@ -45,10 +62,11 @@ while True:
 
     
     cv2.imshow("Reconocimiento de Gestos", frame)
-    
+
     
     if cv2.waitKey(1) == 27:
         break
+
 
 cap.release()
 cv2.destroyAllWindows()
